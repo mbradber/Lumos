389246bf97e8e5da26bccaab6d9f80777c004d09
ROBLOX\mbradberry <mcbradberry@gmail.com>
2018-05-11 18:39:44 -0700

    impl update

--- Lumos/Lumos/main.cpp
+++ Lumos/Lumos/main.cpp
@@ -19,12 +19,8 @@ public:
     void printError(const std::string& variableName, const std::string& reason);
  
     template<typename T>
-    T get(const std::string& variableName) {
-        // will be implemented later in tutorial
-    }
-    bool lua_gettostack(const std::string& variableName) {
-       // will be explained later too
-    }
+	T get(const std::string& variableName);
+	bool lua_gettostack(const std::string& variableName);
  
     // Generic get
     template<typename T>
@@ -39,10 +35,14 @@ public:
     }
 private:
     lua_State* L;
+	int level;
 };
 
-LuaScript::LuaScript(const std::string& filename) {
+LuaScript::LuaScript(const std::string& filename) 
+: level(0)
+{
     L = luaL_newstate();
+	luaL_openlibs(L); /* opens the standard libraries */
     if (luaL_loadfile(L, filename.c_str()) || lua_pcall(L, 0, 0, 0)) {
         std::cout<<"Error: script not loaded ("<<filename<<")"<<std::endl;
         L = 0;
@@ -58,28 +58,72 @@ void LuaScript::printError(const std::string& variableName, const std::string& r
 }
 
 template<>
-inline std::string LuaScript::lua_getdefault() {
+inline std::string LuaScript::lua_getdefault(const std::string& variableName) {
 	return "null";
 }
 
-// TODO: get rid of hard-coded path
-static std::string scriptsPath = "C:\\Users\\mbradberry\\dev\\lumos\\Lumos\\Lumos\\scripts";
-
-int main (void) {
-	char buff[256];
-	int error;
-	lua_State *L = luaL_newstate(); /* opens Lua */
-	luaL_openlibs(L); /* opens the standard libraries */
-
-	std::string testFile = scriptsPath + "\\test.lua";
-	error = luaL_loadfile(L, testFile.c_str());
-	lua_pcall(L, 0, 0, 0);
-	if (error) {
-		fprintf(stderr, "%s", lua_tostring(L, -1));
-		lua_pop(L, 1); /* pop error message from the stack */
+template <typename T>
+T LuaScript::get(const std::string& variableName) {
+	if (!L) {
+		printError(variableName, "Script is not loaded!");
+		return lua_getdefault<T>();
 	}
 
-	lua_close(L);
+	T result;
+	if (lua_gettostack(variableName)) {
+		result = lua_get<T>(variableName);
+	}
+	else {
+		result = lua_getdefault<T>();
+	}
+
+	lua_pop(L, level + 1);
+	return result;
+}
+
+bool LuaScript::lua_gettostack(const std::string& variableName) {
+	level = 0;
+	std::string var = "";
+	for (int i = 0; i < variableName.size(); ++i) {
+		if (variableName.at(i) == '.') {
+			if (level == 0) {
+				lua_getglobal(L, var.c_str());
+			}
+			else {
+				lua_getfield(L, -1, var.c_str());
+			}
+
+			if (lua_isnil(L, -1)) {
+				printError(variableName, var + " is not defined!");
+				return false;
+			}
+			else {
+				var += variableName.at(i);
+			}
+		}
+	}
+
+	if (level == 0) {
+		lua_getglobal(L, var.c_str());
+	}
+	else {
+		lua_getfield(L, -1, var.c_str());
+	}
+
+	if (lua_isnil(L, -1)) {
+		printError(variableName, var + " is not defined!");
+		return false;
+	}
+
+	return true;
+}
+
+// TODO: get rid of hard-coded path
+static std::string scriptsPath = "C:\\Users\\mbradberry\\dev\\lumos\\Lumos\\Lumos\\scripts\\";
+
+int main (void) {
+	std::string script = scriptsPath + "test.lua";
+	LuaScript ls(script);
 
 	getchar();
 	return 0;
--- Lumos/Lumos/scripts/test.lua
+++ Lumos/Lumos/scripts/test.lua
@@ -1,5 +1,1 @@
-local a = 10
-local b = 20
-local c = a + b
-
-print("c = " .. c)
+print('Hello test!')
