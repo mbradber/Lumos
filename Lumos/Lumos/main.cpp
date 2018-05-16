1b22052a0a966797c7d16c727965944302a7cf18
ROBLOX\mbradberry <mcbradberry@gmail.com>
2018-05-15 17:54:09 -0700

    additional lua stuff

--- Lumos/Lumos/main.cpp
+++ Lumos/Lumos/main.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <string>
 
+#include "luawrapperutil.h"
+
 extern "C" {
 #include "lua.h"
 #include "lauxlib.h"
@@ -33,6 +35,13 @@ public:
     T lua_getdefault(const std::string& variableName) {
       return 0;
     }
+
+	inline void clean() {
+		int n = lua_gettop(L);
+		lua_pop(L, n);
+	}
+
+	std::vector<int> getIntVector(const std::string& name);
 private:
     lua_State* L;
 	int level;
@@ -60,6 +69,39 @@ void LuaScript::printError(const std::string& variableName, const std::string& r
 template<>
 inline std::string LuaScript::lua_getdefault(const std::string& variableName) {
 	return "null";
+}
+
+template<>
+inline bool LuaScript::lua_get(const std::string& variableName) {
+	return (bool)lua_toboolean(L, -1);
+}
+
+template<>
+inline float LuaScript::lua_get(const std::string& variableName) {
+	if (!lua_isnumber(L, -1)) {
+		printError(variableName, "Not a number");
+	}
+
+	return (float)lua_tonumber(L, -1);
+}
+
+template <>
+inline int LuaScript::lua_get(const std::string& variableName) {
+    if(!lua_isnumber(L, -1)) {
+      printError(variableName, "Not a number");
+    }
+    return (int)lua_tonumber(L, -1);
+}
+
+template <>
+inline std::string LuaScript::lua_get(const std::string& variableName) {
+    std::string s = "null";
+    if(lua_isstring(L, -1)) {
+      s = std::string(lua_tostring(L, -1));
+    } else {
+      printError(variableName, "Not a string");
+    }
+    return s;
 }
 
 template <typename T>
@@ -116,6 +158,24 @@ bool LuaScript::lua_gettostack(const std::string& variableName) {
 	}
 
 	return true;
+}
+
+std::vector<int> LuaScript::getIntVector(const std::string& name) {
+	std::vector<int> v;
+	lua_gettostack(name);
+	if (lua_isnil(L, -1)) {
+		return std::vector<int>();
+	}
+
+	lua_pushnil(L);
+
+	while (lua_next(L, -2)) {
+		v.push_back((int)lua_tonumber(L, -1));
+		lua_pop(L, 1);
+	}
+
+	clean();
+	return v;
 }
 
 // TODO: get rid of hard-coded path
